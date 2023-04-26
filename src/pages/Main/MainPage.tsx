43a7b2f6c1dc973e524b6223757542243bc908d6
kror-shack <tariqfouzan1@gmail.com>
2023-04-26 12:17:40 +0300

    added city to show on first load

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -7,6 +7,14 @@ import "./MainPage.scss";
 function DisplayPage() {
   const [cityDetails, setCityDetails] = useState<CityDetails | undefined>();
   const [tempUnit, setTempUnit] = useState<Temp>("C");
+
+  useEffect(() => {
+    setCityDetails((prev) => ({
+      ...prev,
+      name: "Munich",
+    }));
+  }, []);
+
   return (
     <div className="Main-Page">
       <Header cityDetails={cityDetails} setCityDetails={setCityDetails} />
