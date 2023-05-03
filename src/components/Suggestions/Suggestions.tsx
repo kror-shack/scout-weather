47b3c9638873b789b9a286d5dd1e48c1f73e7708
kror-shack <tariqfouzan1@gmail.com>
2023-05-03 10:10:44 +0300

    Refactor
    refactored the cases to use one function

--- src/components/Suggestions/Suggestions.tsx
+++ src/components/Suggestions/Suggestions.tsx
@@ -42,137 +42,99 @@ const Suggestions = ({
     }
   }
 
+  function addTempSuggestion(sugg: string) {
+    setSuggestions((prevSuggestions) => ({
+      ...prevSuggestions,
+      tempSuggestion: sugg,
+    }));
+  }
+
+  function addUVSuggestion(sugg: string) {
+    setSuggestions((prevSuggestion) => ({
+      ...prevSuggestion,
+      uvSuggestion: sugg,
+    }));
+  }
+
   useEffect(() => {
     switch (weatherCode) {
       case 0:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Wear sunglasses and sunscreen",
-        }));
+        addTempSuggestion("Wear sunglasses and sunscreen");
         break;
       case 1:
       case 2:
       case 3:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Wear a light jacket",
-        }));
+        addTempSuggestion("Wear a light jacket");
         break;
       case 45:
       case 48:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Be cautious while driving and use headlights",
-        }));
+        addTempSuggestion("Be cautious while driving and use headlights");
         break;
       case 51:
       case 53:
       case 55:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Carry an umbrella",
-        }));
+        addTempSuggestion("Carry an umbrella");
         break;
       case 56:
       case 57:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Be cautious while driving and avoid icy patches",
-        }));
+        addTempSuggestion("Be cautious while driving and avoid icy patches");
         break;
       case 61:
       case 63:
       case 65:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Carry a raincoat and wear waterproof shoes",
-        }));
+        addTempSuggestion("Carry a raincoat and wear waterproof shoes");
         break;
       case 66:
       case 67:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Be cautious while driving and avoid icy patches",
-        }));
+        addTempSuggestion("Be cautious while driving and avoid icy patches");
         break;
       case 71:
       case 73:
       case 75:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Wear warm clothes and boots",
-        }));
+        addTempSuggestion("Wear warm clothes and boots");
         break;
       case 77:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Be cautious while driving and avoid icy patches",
-        }));
+        addTempSuggestion("Be cautious while driving and avoid icy patches");
         break;
       case 80:
       case 81:
       case 82:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Carry an umbrella and wear waterproof shoes",
-        }));
+        addTempSuggestion("Carry an umbrella and wear waterproof shoes");
         break;
       case 85:
       case 86:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Wear warm clothes and boots",
-        }));
+        addTempSuggestion("Wear warm clothes and boots");
         break;
       case 95:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Stay indoors and avoid metal objects",
-        }));
+        addTempSuggestion("Stay indoors and avoid metal objects");
         break;
       case 96:
       case 99:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Stay indoors and park your car under cover",
-        }));
+        addTempSuggestion("Stay indoors and park your car under cover");
         break;
       default:
-        setSuggestions((prevSuggestions) => ({
-          ...prevSuggestions,
-          tempSuggestion: "Check the weather forecast before going outside",
-        }));
+        addTempSuggestion("Check the weather forecast before going outside");
     }
   }, [weatherCode]);
 
   useEffect(() => {
     switch (uvIndex) {
       case "Low":
-        setSuggestions((prevSuggestion) => ({
-          ...prevSuggestion,
-          uvSuggestion: "Enjoy the sun, but remember to stay hydrated.",
-        }));
+        addUVSuggestion("Enjoy the sun, but remember to stay hydrated.");
         break;
       case "Moderate":
-        setSuggestions((prevSuggestion) => ({
-          ...prevSuggestion,
-          uvSuggestion:
-            "Stay in the shade during midday hours and wear light clothing.",
-        }));
+        addUVSuggestion(
+          "Stay in the shade during midday hours and wear light clothing."
+        );
         break;
       case "High":
-        setSuggestions((prevSuggestion) => ({
-          ...prevSuggestion,
-          uvSuggestion:
-            "Limit time in the sun and avoid being outside during midday hours.",
-        }));
+        addUVSuggestion(
+          "Limit time in the sun and avoid being outside during midday hours."
+        );
         break;
       default:
         console.log(uvIndex);
-        setSuggestions((prevSuggestion) => ({
-          ...prevSuggestion,
-          uvSuggestion: "Check the UV index before going outside.",
-        }));
+        addUVSuggestion("Check the UV index before going outside.");
     }
   }, [uvIndex]);
 
